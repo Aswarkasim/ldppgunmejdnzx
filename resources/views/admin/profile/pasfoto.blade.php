05365649d92db937824f5c39d42b04e6089d2314
aswarkasim <aswarkasim@gmail.com>
2022-07-12 07:59:50 +0700

    pasfoto

--- resources/views/admin/profile/pasfoto.blade.php
+++ resources/views/admin/profile/pasfoto.blade.php
@@ -12,7 +12,7 @@
         </div>
         <div class="col-md-9">
           <input type="file" class="form-control @error('pasfoto') is-invalid @enderror" name="pasfoto"  value="{{isset($profile) ? $profile->pasfoto : old('pasfoto')}}" required placeholder="Akreditasi Sekolah">
-          <small>Pastika foto berukuran 3X4 dan ukuran maksimal 200KB</small>
+          <small>Pastikan foto berukuran 3X4 dan ukuran maksimal 200KB</small>
 
           @error('pasfoto')
           <div class="invalid-feedback">
@@ -29,6 +29,17 @@
       
     </div>
     
+ </div>
+
+ <div class="col-md-6">
+    <h3>Ketentuan</h3>
+    <ol type="1">
+      <li>Format Jpg dengan dimensi 720 x 480 pixel</li>
+      <li>Ukuran maksimal 200 kb dengan</li>
+      <li>Bagi laki-laki wajib menggunakan jas hitam, kemeja putih polos dan berkerah serta dasi hitam</li>
+      <li>Bagi perempuan wajib menggunakan jas hitam, kemeja putih polos dan berkerah serta kerudung putih (bagi yang berkerudung)</li>
+      <li>Background berwarna merah perempuan, warna biru laki-laki nama file: nomor UKG/Peg.ID.- NAMA - FOTO contoh: 201500731653 - DWI ARIYANTI - FOTO</li>
+    </ol>
  </div>
 </div>
 
