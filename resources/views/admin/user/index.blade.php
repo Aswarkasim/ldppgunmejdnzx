fefca244bb6ad2ade7fdf1934b10065ae32a599a
aswarkasim <aswarkasim@gmail.com>
2022-07-14 14:11:53 +0700

    mahasiswa

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -7,7 +7,7 @@
     <form action="" method="get">
     <div class="input-group input-group-sm">
         <input type="hidden" name="role" value="{{request('role')}}" class="form-control" placeholder="Cari..">
-        <input type="text" name="cari" class="form-control" placeholder="Cari..">
+        <input type="text" name="cari" class="form-control" placeholder="Cari username atau nomor UKG">
         <span class="input-group-append">
           <button type="submit" class="btn btn-primary btn-flat"><i class="fa fa-search"></i></button>
           <a href="/admin/user" class="btn btn-info btn-flat tombol-hapus"><i class="fa fa-sync-alt"></i></a>
