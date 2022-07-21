bfa54a23e7583386b47bb7058cac14fca5e0c59c
aswarkasim <aswarkasim@gmail.com>
2022-07-21 08:57:14 +0700

    adakah

--- resources/views/admin/mahasiswa/index.blade.php
+++ resources/views/admin/mahasiswa/index.blade.php
@@ -16,7 +16,7 @@
   <div class="float-right">
     <form action="" method="get">
     <div class="input-group input-group-sm">
-        <input type="text" name="cari" class="form-control" placeholder="Cari..">
+        <input type="text" name="cari" class="form-control" placeholder="Cari nama atau nomor ukg">
         <span class="input-group-append">
           <button type="submit" class="btn btn-primary btn-flat"><i class="fa fa-search"></i></button>
           <a href="/account/mahasiswa" class="btn btn-info btn-flat"><i class="fa fa-sync-alt"></i></a>
