c842aeaf05f1282844cbb3857ac618fe425990ce
unknown <aswarkasim@gmail.com>
2022-09-19 18:57:40 +0700

    aa

--- app/Http/Controllers/AdminPenilaianController.php
+++ app/Http/Controllers/AdminPenilaianController.php
@@ -49,6 +49,7 @@ class AdminPenilaianController extends Controller
         }
         // $mahasiswa = Mahasiswa::wherePeriodeId($periode_id)->whereStatus('VALID')->paginate(10);
         $kelas_peserta = KelasPeserta::whereKelasId($kelas_id)->get();
+        dd($kelas_peserta);
         foreach ($kelas_peserta as $item) {
             $cek = Nilai::whereNoUkg($item->no_ukg)->whereMatakuliahId($matakuliah_id)->first();
             if ($cek == false) {
